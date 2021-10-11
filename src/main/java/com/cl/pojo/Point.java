fd6974ac234c60aa652d7682bdec7e75a9e5e5f7
DJLobsterLover <1281899599@qq.com>
2021-10-11 18:31:36 +0800

    hello

--- src/main/java/com/cl/pojo/Point.java
+++ src/main/java/com/cl/pojo/Point.java
@@ -1,2 +1,58 @@
-package com.cl.pojo;public class Point {
+package com.cl.pojo;
+
+/**
+ * @author DJLobster
+ */
+public class Point {
+    private double x;
+    private double y;
+    private double z;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+    }
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point() {
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
 }
