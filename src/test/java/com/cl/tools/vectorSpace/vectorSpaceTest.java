06ca61808275c1040ee6c62f229eade47aa45d17
DJLobsterLover <1281899599@qq.com>
2021-10-12 10:33:28 +0800

    hell

--- src/test/java/com/cl/tools/vectorSpace/vectorSpaceTest.java
+++ src/test/java/com/cl/tools/vectorSpace/vectorSpaceTest.java
@@ -14,13 +14,15 @@ public class vectorSpaceTest {
     public void vectorSpaceTest(){
         List<Point> points = new ArrayList<Point>();
         Point p1 = new Point(0,0);
-        Point p2 = new Point(1,1);
-        Point p3 = new Point(2,0);
-        Point p4 = new Point(3,1);
+        Point p2 = new Point(0,2);
+        Point p3 = new Point(2,2);
+        Point p4 = new Point(1,1);
+        Point p5 = new Point(2,0);
         points.add(p1);
         points.add(p2);
-        points.add(p4);
         points.add(p3);
+        points.add(p4);
+        points.add(p5);
         VectorSpaceCal vs = new VectorSpaceCalImpl();
         double r = vs.vectorSpaceCal(points);
         System.out.println(r);
